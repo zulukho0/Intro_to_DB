14acb5435babc2e0fbe03f1f672815bb8bb27808
skroof <zulukho0@gmail.com>
2025-08-17 15:53:01 +0200

    fix spelling of address

--- task_5.sql
+++ task_5.sql
@@ -1,3 +1,3 @@
 USE alx_book_store;
 
-INSERT INTO customers(customer_id, customer_name, email, address) values(1, "Cole Baidoo", "cbaidoo@sandtech.com", "123 Hapinnes Ave.")
+INSERT INTO customers(customer_id, customer_name, email, address) values(1, "Cole Baidoo", "cbaidoo@sandtech.com", "123 Happinnes Ave.")
