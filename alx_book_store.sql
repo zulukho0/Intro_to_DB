bd3eb531453a6037065c9c5687a1c0ab51c0e47d
skroof <zulukho0@gmail.com>
2025-08-17 14:50:11 +0200

    refactor: Capitalise table names in fk referencing

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -22,13 +22,13 @@ CREATE TABLE IF NOT EXISTS Orders(
     order_id INT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
-    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 CREATE TABLE IF NOT EXISTS Order_Details(
     orderdetailid INT PRIMARY KEY,
     order_id INT,
     book_id INT,
     quantity DOUBLE,
-    FOREIGN KEY(order_id) REFERENCES orders(order_id),
-    FOREIGN KEY(book_id) REFERENCES books(book_id)
+    FOREIGN KEY(order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY(book_id) REFERENCES Books(book_id)
 );
