288807ddf38b13d987b7205446970ba2a42e6ab7
skroof <zulukho0@gmail.com>
2025-08-17 15:52:16 +0200

    fix capital;ised words

--- task_5.sql
+++ task_5.sql
@@ -1,3 +1,3 @@
 USE alx_book_store;
 
-INSERT INTO Customers(Customer_id, Customer_name, email, address) values(1, "Cole Baidoo", "cbaidoo@sandtech.com", "123 Hapinnes Ave.")
+INSERT INTO customers(customer_id, customer_name, email, address) values(1, "Cole Baidoo", "cbaidoo@sandtech.com", "123 Hapinnes Ave.")
