32f5e64085f99c2a92c5d4b1f9399cdbe877a46f
skroof <zulukho0@gmail.com>
2025-08-17 14:51:22 +0200

    refactor: fix referencing

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -29,6 +29,6 @@ CREATE TABLE IF NOT EXISTS Order_Details(
     order_id INT,
     book_id INT,
     quantity DOUBLE,
-    FOREIGN KEY(order_id) REFERENCES Orders(order_id),
-    FOREIGN KEY(book_id) REFERENCES Books(book_id)
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
