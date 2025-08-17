c15da661f6661d2a9a2e8cfab5f0d199a0129a50
skroof <zulukho0@gmail.com>
2025-08-17 14:48:45 +0200

    refacroe: capitalise all names of tables

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS Authors(
     author_id INT PRIMARY KEY,
     author_name VARCHAR(215)
 );
-CREATE TABLE IF NOT EXISTS books(
+CREATE TABLE IF NOT EXISTS Books(
     book_id INT PRIMARY KEY,
     title VARCHAR(130),
     author_id INT,
@@ -12,19 +12,19 @@ CREATE TABLE IF NOT EXISTS books(
     publication_date DATE,
     FOREIGN KEY(Author_id) REFERENCES authors(author_Id)
 );
-CREATE TABLE IF NOT EXISTS customers(
+CREATE TABLE IF NOT EXISTS Customers(
     customer_id INT PRIMARY KEY,
     customer_name VARCHAR(215),
     email VARCHAR(215),
     address TEXT
 );
-CREATE TABLE IF NOT EXISTS orders(
+CREATE TABLE IF NOT EXISTS Orders(
     order_id INT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
     FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
 );
-CREATE TABLE IF NOT EXISTS order_details(
+CREATE TABLE IF NOT EXISTS Order_Details(
     orderdetailid INT PRIMARY KEY,
     order_id INT,
     book_id INT,
